e850d7575d4c61fabd26a2c6905ac9996c99c082
henryaf0519 <henryaf0519@gmail.com>
2025-08-28 17:48:37 -0500

    fix: remove CORS configuration from main.ts and adjust app initialization

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'reflect-metadata';
 import cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const whitelist = [
-    'http://localhost:5173',
-    'https://orvexchat-666d6.web.app',
-  ];
-  app.enableCors({
-    origin: function (
-      origin: string | undefined,
-      callback: (err: Error | null, allow?: boolean) => void,
-    ) {
-      // Permitir solicitudes sin origen (como Postman o peticiones de servidor a servidor)
-      // o si el origen está en nuestra lista blanca.
-      if (!origin || whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('No permitido por CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy', 1);
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
