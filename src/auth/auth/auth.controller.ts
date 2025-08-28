1cb404cab810a489a8fc4b285e1457221b79577c
henryaf0519 <henryaf0519@gmail.com>
2025-08-28 14:51:27 -0500

    fix: update cookie settings to use 'none' for sameSite and specify domain

--- src/auth/auth/auth.controller.ts
+++ src/auth/auth/auth.controller.ts
@@ -36,7 +36,8 @@ export class AuthController {
     res.cookie('accessToken', loginData.access_token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: 'none',
+      domain: '.pruebasfaciles.online',
       path: '/',
       expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
     });
